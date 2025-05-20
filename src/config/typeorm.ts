a39992b3ef32cf00809ed3b25c241195ecf3ba7d
Valmir Thume Junior <juninhothume21@gmail.com>
2025-05-19 23:35:14 -0300

    fix: update database credentials to use environment variables

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,16 +3,15 @@ import { Variety } from '../modules/products/infra/typeorm/entities/Variety';
 import { CreateProduct1747359270854 } from '../shared/typeorm/migrations/1747359270854-CreateProduct';
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-// import env from './env';
-// import { Product } from '@products/infra/typeorm/entities/Product';
+import env from './env';
 
 export const dataSource = new DataSource({
   type: 'postgres',
   database: 'eCommerce-project',
   host: 'localhost',
   port: 5432,
-  username: 'postgres',
-  password: 'admin',
+  username: env.USER_DB,
+  password: env.USER_DB_PASSWORD,
   synchronize: false,
   logging: false,
   entities: ['src/**/entities/*.ts'],
